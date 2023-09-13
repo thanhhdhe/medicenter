ee80f9acf3426a3912f0af9d2a81e4571640f31c
BlinkWork <hbichung@yahoo.com>
2023-09-13 23:51:25 +0700

    create login controller

--- src/java/Database/UserDAO.java
+++ src/java/Database/UserDAO.java
@@ -8,21 +8,37 @@ package Database;
  *
  * @author pc
  */
-public class UserDAO extends MyDAO{
-    
+public class UserDAO extends MyDAO {
+
     //reset password by User ID(ten dang nhap)
-    public void resetPassword(String newPassword, String UserID){
-        xSql="update Student set Password= ? where UserID= ?";
-        try {      
-        ps = con.prepareStatement(xSql);
-        ps.setString(1, newPassword);
-        ps.setString(2, UserID);
-        ps.executeUpdate();
-        ps.close();
-     }
-      catch(Exception e) {
-        e.printStackTrace();
-      }
+    public void resetPassword(String newPassword, String UserID) {
+        xSql = "update Student set Password= ? where UserID= ?";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, newPassword);
+            ps.setString(2, UserID);
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
+    public boolean loginAccount(String email, String password) {
+        xSql = "select * from Users where email = ? and password = ?";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, email);
+            ps.setString(2, password);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
---
+++ src/java/controller/LoginController.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import Database.UserDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author hbich
+ */
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        UserDAO u = new UserDAO();
+        if (u.loginAccount(email, password)) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("email", email);
+            response.sendRedirect(request.getContextPath());
+        }
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
