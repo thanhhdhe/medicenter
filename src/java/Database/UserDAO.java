81c94aad14be267a788d4310d30ecba355e138d1
Hiếu Chung <chtalong@gmail.com>
2023-09-18 08:48:08 +0000

    Update UserDAO.java

--- src/java/Database/UserDAO.java
+++ src/java/Database/UserDAO.java
@@ -4,6 +4,9 @@
  */
 package Database;
 
+import java.sql.Date;
+import model.User;
+
 /**
  *
  * @author pc
@@ -23,7 +26,7 @@ public class UserDAO extends MyDAO {
             e.printStackTrace();
         }
     }
-    
+
     public boolean loginAccount(String email, String password) {
         xSql = "select * from [dbo].[Users] where email = ? and password = ?";
         try {
@@ -39,6 +42,55 @@ public class UserDAO extends MyDAO {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public User selectUser(String email) {
+        User u = null;
+        xSql = "select * from [dbo].[Users] where email = ?";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, email);
+
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int userID = rs.getInt("userID");
+                String userName = rs.getString("userName");
+                String password = rs.getString("password");
+                String Email = rs.getString("email");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                String gender = rs.getString("gender");
+                Date birthday = rs.getDate("birthday");
+                String phoneNumber = rs.getString("phoneNumber");
+                String profileImage = rs.getString("profileImage");
+                String role = rs.getString("role");
+                String address = rs.getString("address");
+                u = new User(userID, userName, password, Email, firstName, lastName, gender, birthday, phoneNumber, profileImage, role, address);
+            }
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return u;
+    }
+
+    public void insert(User x) {
+        xSql = "INSERT INTO [dbo].[User]([email],[firstname],[lastname],[password],[gender],[address],[phonenumber])"
+                + "     VALUES(?,?,?,?,?,?,?)";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, x.getEmail());
+            ps.setString(2, x.getFirstName());
+            ps.setString(3, x.getLastName());
+            ps.setString(4, x.getPassword());
+            ps.setString(5, x.getGender());
+            ps.setString(6, x.getAddress());
+            ps.setString(7, x.getPhoneNumber());
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
