b50554706446f631053260da1b9673937bd845a9
Quan <quannmhe171875@fpt.edu.vn>
2023-10-26 22:13:44 +0700

    fix doc detail

--- src/java/Database/StaffDAO.java
+++ src/java/Database/StaffDAO.java
@@ -160,7 +160,7 @@ public class StaffDAO extends MyDAO {
                 String professionalAchievements = rs.getString("ProfessionalAchievements");
                 String specializedActivities = rs.getString("SpecializedActivities");
                 
-                staff = new Staff(staffID, staffName, password, email, fullName, gender, phoneNumber, profileImage, role, rank, specialty, introduction);
+                staff = new Staff(staffID, fullName, gender, phoneNumber, profileImage, rank, specialty, introduction, specializedActivities, professionalAchievements, depthStudy);
             }
             rs.close();
             ps.close();
